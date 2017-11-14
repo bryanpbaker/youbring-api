555a0b911bf8835071d5d555d67f045de9a1fb6a
Bryan Baker <bryanpbaker@gmail.com>
2017-11-13 18:56:43 -0600

    added get events and contacts end point

---
+++ controllers/userContactCtrl.js
@@ -0,0 +1,10 @@
+const User = require('../models/userModel');
+
+module.exports = async (req, res) => {
+  const user = await User.findUserById(req.params.id);
+
+  res.json({
+    success: true,
+    contacts: user.contacts,
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authGuard = require('../services/authGuard');
 const userCtrl = require('../controllers/userCtrl');
+const userContactCtrl = require('../controllers/userContactCtrl');
+const userEventsCtrl = require('../controllers/userEventsCtrl');
 
 router.use('/', authGuard);
 
 router.get('/:id', userCtrl);
+
+router.get('/:id/contacts', userContactCtrl);
+
+router.get('/:id/events', userEventsCtrl);
 
 module.exports = router;
