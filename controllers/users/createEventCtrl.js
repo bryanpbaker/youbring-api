6c45f2d344bff525f261a4a6777526ff99a419f2
Bryan Baker <bryanpbaker@gmail.com>
2018-01-02 15:44:33 -0600

    WIP event creation

--- controllers/users/createEventCtrl.js
+++ controllers/users/createEventCtrl.js
@@ -9,7 +9,14 @@ module.exports = async (req, res) => {
     user.save();
     res.json({
       success: true,
-      events: user.events,
+      profile: {
+        userId: user.userId,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        events: user.events,
+        contact: user.contacts,
+      },
     });
   } else {
     res.status(404).json({
